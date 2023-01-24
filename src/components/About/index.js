05e93b17151aae1e9f780121bde8c3ef3628d2c2
dmadd1996 <94644880+dmadd1996@users.noreply.github.com>
2023-01-24 11:38:15 -0600

    added hover functionality to image previews

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -2,9 +2,9 @@ import React from 'react';
 import coverImage from '../../assets/cover/cover-image.jpg';
 function About() {
   return (
-    <section className="my-5">
+    <section className="my-5" >
       <h1 id="about">Who am I?</h1>
-      <img src={coverImage} className="my-2" style={{ width: "100%" }} alt="cover" />
+      <img src={coverImage} className="my-2" style={{ width: "25%" }} alt="cover" />
       <div className="my-2">
         <p>
         Deep love of complex problems and elegant solutions. A lifelong learner, I am inspired to create computer systems that bring my background in psychology to the forefront of intuitive web design. My experiences include general IT, front and back-end web design, server development and maintenance. Being a team-player and a fair leader are the standards with which I measure my character.
