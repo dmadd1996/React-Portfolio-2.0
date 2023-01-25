020823658ba21410c7eb695b2490f813a12da000
dmadd1996 <94644880+dmadd1996@users.noreply.github.com>
2023-01-25 17:00:04 -0600

    improved on gallery descriptions that go in the modals

--- src/components/PhotoList/index.js
+++ src/components/PhotoList/index.js
@@ -10,7 +10,7 @@ const PhotoList = ({ category }) => {
       name: 'Note Taker',
       category: 'backend',
       description:
-        'Using Express.js back end framework, I created a project that saved notes to an express server for later recall. With a snappy front end UI and a straightforward call to display note data, it showcases my initial foray into back end development. It was deployed to heroku.',
+        'Using Express.js back end framework, I built an app that uses CRUD API calls to keep user notes. It has a snappy front end UI and a straightforward, internal API call to parse new notes into JSON, array them in the database and re-stringify them onto the UI. This website showcases my initial foray into back end development. It is currently live as a public message board. Click on the image to leave a message!',
       link: "https://github.com/dmadd1996/Note-Taker",
       preview:  "https://note-taker-dmadd1996.herokuapp.com/"
     },
@@ -18,7 +18,7 @@ const PhotoList = ({ category }) => {
       name: 'Social Network API',
       category: 'backend',
       description:
-        'Using backend Javascript and insomnia for API calls, I set up the API framework for a user to pull, post, edit and delete entries to a social media platform. Link takes you to a youtube video outlining the function of the code.',
+        "Knowledge of SQL is important, but in today's data-driven world, a developer needs newer, faster tools. NoSQL databases can process huge volumes of data in a fraction of the time, which is why it has become an industry standard. In this application, I used MongoDB as my NoSQL framework. Social networks are constantly being updated and added to, so agile development standards are a must to account for this unstructured data. Using the Insomnia API platform, the video preview exhibits how a front-end UI would make internal CRUD API calls to create, read, update and delete user posts. Click on the image to view the video!",
         link: 'https://github.com/dmadd1996/social-network-api',
         preview:'https://www.youtube.com/watch?v=qTjoFmLylBIab_channel=DerekMadderom',
     },
@@ -26,7 +26,7 @@ const PhotoList = ({ category }) => {
       name: 'Employee Tracker',
       category: 'backend',
       description:
-        'Using a SQL database and inquirer.js, the user is able to build a team of employees, connected via foreign keys to their respective departments, job titles, salaries and managers. Translating from inquirer input to SQL syntax, the user is able to edit, add and delete fields from the command line.',
+        "Sometimes old SQL just gets the job done. When a project calls for a system that can easily store textual data that must be easily accessible to non-developers, the stability and simplicity of SQL Content Management Systems is unmatched. For this back-end application, a company's departments, roles, employees and salaries are connected via foreign keys. Combine that with an Inquirer Node package as a command line interface, and it can be securely viewed, and edited, through easy to use text prompts. This system could easily be expanded to a full-stack application that makes onboarding and presenting employee data simple and intuitive. Click on the image to see how inquirer can make interfacing with SQL a breeze!",
         link: 'https://github.com/dmadd1996/Employee-Tracker',
         preview: 'https://youtu.be/hLGCesRoTNM'
     },
@@ -34,7 +34,7 @@ const PhotoList = ({ category }) => {
       name: 'E-Commerce Server',
       category: 'backend',
       description:
-        'As part of my coding boot camp, I was tasked with creating a functional e-commerce database. Using the Object Relational Mapping technique, which makes use of virtual objects to organize and compile large amounts of data in a fraction of the time compared to traditional databases.',
+        'Internet retail is the largest sector of the electronics industry, generating trillions of dollars every year in revenue. For any business, developers need a fundamental understanding of E-commerce architecture, due to its near universal prevalence. For this project, using an Express.js API and configuring it to use Sequelize, seeded commercial data (categories, products and tags) can be injected into a MySQL database. The Object-Relational Maps use API routes to perform RESTful CRUD operations, making huge volumes of data easily searched, narrowed and organized. This many-to-many relationship structure allows clients to find exactly what they are searching for without having to sift through mountains of data. Click on the image to watch how I use the Insomnia API platform to perform multiple variations of each CRUD operation and interact with the ORM!',
       link: 'https://github.com/dmadd1996/E-Commerce/tree/main/Develop',
       preview: "https://youtu.be/M2GKG4dpNLs"
     },
@@ -42,7 +42,7 @@ const PhotoList = ({ category }) => {
       name: 'Readme Generator',
       category: 'backend',
       description:
-        'Using Node.js to run programs from the command line interface, I developed a program which captures user inputs and outputs Readme documents that adhere to industry standard formatting. All user input is taken from the CLI, with no browser necessary, making this my first backend development project.',
+        'Using Node.js to and Inquirer.js from the command line interface, I developed a program which captures user inputs and outputs Readme documents that adhere to industry standard formatting. All user input is taken from the CLI, with no browser necessary, making this my first backend development project.',
       link: 'https://github.com/dmadd1996/ReadME_Generator',
       preview: 'https://youtu.be/C4lib3Z4LHc'
      },
@@ -51,7 +51,7 @@ const PhotoList = ({ category }) => {
       name: 'App-Showcase',
       category: 'frontend',
       description:
-        `This is one of the first projects I ever worked on. It showcases my ability to make a straightforward website that acts as a hypothetical portfolio. This application serves to contrast this site, which is an actual portfolio of my projects and showcases how far I have come as a developer.`,
+        `This is one of the first projects I ever worked on. It showcases my ability to make a straightforward website that acts as a hypothetical portfolio. This application serves to contrast my current portfolio, which showcases how far I have come as a developer. It was built with raw HTML and CSS.`,
       link: 'https://github.com/dmadd1996/App_Showcase',
       preview: "https://dmadd1996.github.io/App_Showcase/"
     },
@@ -59,7 +59,7 @@ const PhotoList = ({ category }) => {
       name: 'Password Generator',
       category: 'frontend',
       description:
-        'This project made use of javascript to allow users to select length, and use of case, numeric and special characters for a generated password. At the push of a button, the defined password is created for the user to copy and deploy.',
+        'This project made use of JavaScript to allow users to select length, and use of case, numeric and special characters for a generated password. At the push of a button, the defined password is created for the user to copy and deploy. It was my first use of JavaScript to make dynamic front-end applications. While all of the JavaScript is original code, I made my own brute force regex to make sure that the generated password contained one of each defined character. It was not required for the project, but I enjoyed teaching myself something new through trial and error. Click on the image to generate a password for yourself!',
         link:"https://github.com/dmadd1996/Password_Generator",
         preview:"https://dmadd1996.github.io/Password_Generator/"
     },
@@ -67,15 +67,15 @@ const PhotoList = ({ category }) => {
       name: 'JavaScript Quiz',
       category: 'frontend',
       description:
-        'This quiz utilized the basics of Javascript to provide a timed test. Each question is presented on a card that is pulled from an array. When the user inputs an incorrect answer, time is deducted from a timer. If the user answers correctly, the next question card is displayed. At the end of the quiz, the user is able to record their scores and save them to the local storage of their browser.',
+        'This quiz utilized the basics of Javascript to provide a timed test. Each question is presented on a card that is pulled from an array. When the user inputs an incorrect answer, time is deducted from a timer. If the user answers correctly, the next question card is displayed. At the end of the quiz, the user is able to record their scores and save them to the browser. The scores are JSON parsed/stringified to be displayed from the local storage and Bootstrap was used to streamline the styling. This application was a turning point for me in understanding Object-Based Programming, and though it uses basic web design principles, the final product is something for which I am very proud. Click the image to take the quiz!',
       link: "https://github.com/dmadd1996/JS_Quiz",
       preview: "https://dmadd1996.github.io/JS_Quiz/"
     },
     {
-      name: 'Workday Scheduler',
+      name: 'Work Day Scheduler',
       category: 'frontend',
       description:
-        'In this project, I used a third-party api call to monitor the time and allow the user to click on a time slot, add a task (having it save to the local storage) and see the past time slots (gray), the current slot (green) and the future slots (red).',
+        'In this project, I used HTML and CSS (Bootstrap), powered by JQuery to create a workday scheduler app. The program calls to a Third-Party API, named Moment.js to track the local time. DOM traversals, handled by JQuery, allow the user to click on a time slot, turning it into a text entry field, add their task, and save it. The string is converted into JSON and arrayed in the local storage so the application can be closed and reopened without losing tasks. The page is updated dynamically to display the current time and change the color of each section to present past, present and future slots for a given day. This could be expanded to set email alerts, track time on task, or make day/night themes for an application. Click on the image to use the Scheduler!',
         link:"https://github.com/dmadd1996/Work_Calendar",
         preview:"https://dmadd1996.github.io/Work_Calendar/"
     },
@@ -83,7 +83,7 @@ const PhotoList = ({ category }) => {
       name: 'Weather Tracker',
       category: 'frontend',
       description:
-        'This project, using server-side api calls, allows the user to get a breakdown of the weather in any city of their choosing. The city they type into the search bar is inserted into a fetch call and the data is translated onto the screen to provide a full breakdown of the weather in that location. Previous searches are saved in the local storage and displayed on the screen.',
+        'Using a Server-Side API, named OpenWeather, this application allows the user to get weather information in any city of their choosing. The city they type into the search bar is inserted into a Fetch  API call and the pulled data is translated onto the screen to provide a full breakdown of the weather in that location. Previous searches are saved in the local storage and displayed on the screen for ease of access. This could be used on any webpage to display useful weather information for the user. Click on the image to check the weather in your area!',
         link:"https://github.com/dmadd1996/weather_tracker",
         preview:"https://dmadd1996.github.io/weather_tracker/"
     }
