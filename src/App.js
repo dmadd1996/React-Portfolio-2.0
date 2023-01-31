888809f75067ce283897abf550e00b2c2cffb514
dmadd1996 <94644880+dmadd1996@users.noreply.github.com>
2023-01-30 19:03:43 -0600

    added animations and icons for linkedin and git contacts

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import Nav from './components/Nav';
-import About from './components/About';
+// import About from './components/About';
 import Gallery from './components/Gallery';
 import ContactForm from './components/Contact';
 
--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -2,6 +2,18 @@ import React, { useState } from 'react';
 import coverImage from '../../assets/cover/cover-image.jpg';
 import { validateEmail } from '../../utils/helpers';
 
+import anime from 'animejs/lib/anime.es.js';
+var contactIcon = document.getElementsByClassName('contactIcon')
+
+anime({
+  targets: contactIcon,
+  translateY: -40,
+  duration: 5000,
+  direction: 'alternate',
+  loop: true,
+  delay: anime.stagger(1000)
+})
+
 function ContactForm() {
   const [formState, setFormState] = useState({ name: '', email: '', message: '' });
 
@@ -38,16 +50,21 @@ function ContactForm() {
 
   return (
     <section className="my-5" >
-      <h1 id="about">Who am I?</h1>
-      <div>
-        <img src={coverImage} className="my-2" style={{ width: "100%" }} alt="cover" />
-        <div className="my-2">
+      <div className='container'>
+        <h1 id="about">Who am I?</h1>
+        <img src={coverImage} className="my-2 aboutImage" alt="cover" />
+        <div className="my-2 aboutDescription">
           <p>
             Deep love of complex problems and elegant solutions. A lifelong learner, I am inspired to create computer systems that bring my background in psychology to the forefront of intuitive web design. My experiences include general IT, front and back-end web design, server development and maintenance. Being a team-player and a fair leader are the standards with which I measure my character.
           </p>
         </div>
+        <div className='container-2'>
+          <div><img className='contactIcon' src='https://www.maryville.edu/wp-content/uploads/2015/11/Linkedin-logo-1-550x550-300x300.png'></img></div>
+          <div><img className='contactIcon' src='https://cdn-icons-png.flaticon.com/512/5968/5968866.png'></img></div>
+        </div>
       </div>
-      <h1 data-testid="h1tag">Contact me</h1>
+
+      {/* <h1 data-testid="h1tag">Contact me</h1>
       <form id="contact-form" onSubmit={handleSubmit}>
         <div>
           <label htmlFor="name">Name:</label>
@@ -67,7 +84,7 @@ function ContactForm() {
           </div>
         )}
         <button data-testid="button" type="submit">Submit</button>
-      </form>
+      </form> */}
     </section>
   );
 }
